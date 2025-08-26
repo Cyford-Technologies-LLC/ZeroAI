0dd0342a8178c3cb74694b41584a4c9337a86fc6
Allen Ford <allen@cyfordtechnologies.com>
2025-08-26 16:56:56 -0400

    completed docker  for AI Systems..   now compatable everywhere

--- setup-docker.sh
+++ setup-docker.sh
@@ -2,5 +2,15 @@
 
 . setup/setup_docker.sh
 docker compose -f docker-compose.yml -p zeroai-prod down
-docker compose -f docker-compose.yml -p zeroai-prod up --build -d
-docker compose -f docker-compose.learning.yml -p zeroai-learning up --build -d
+
+
+if lspci | grep -i 'NVIDIA' > /dev/null; then
+    echo "NVIDIA GPU detected."
+    docker compose -f docker-compose.yml -f docker-compose.gpu.override.yml -p zeroai-prod up --build -d
+    docker compose -f docker-compose.learning.yml  -f docker-compose.gpu.override.yml -p zeroai-learning up --build -d
+else
+    echo "No NVIDIA GPU found."
+    docker compose -f docker-compose.yml -p zeroai-prod up --build -d
+    docker compose -f docker-compose.learning.yml -p zeroai-learning up --build -d
+fi
+
