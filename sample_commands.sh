cc395f004d1e444acf932083a4c413d2920abfe0
Allen Ford <allen@cyfordtechnologies.com>
2025-08-26 19:20:36 -0400

    completed docker  for AI Systems..   now compatable everywhere

--- sample_commands.sh
+++ sample_commands.sh
@@ -1,5 +1,12 @@
+# download models
+for model in llama3.2:latest mixtral-8x7b-instruct-v0.1 gemma2:2b codellama:7b llava:7b; do
+  ollama pull $model
+done
+
+
 # Add peers
 python3 examples/peer_manager.py  --ip 0.0.0.0 --name GPU-01 --model codellama:13b add
+python3 examples/peer_manager.py  --ip 0.0.0.0 --name GPU-01 --model codellama:13b list
 
 #cli  direct command
 clear ; python3 run/examples/simple_chat.py
