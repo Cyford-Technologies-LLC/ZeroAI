ff73e5e9e0e9be9728bc84bbe74f31572a2a49f0
Allen Ford <allen@cyfordtechnologies.com>
2025-08-28 14:26:20 -0400

    fixing api

--- src/crews/coding/crew.py
+++ src/crews/coding/crew.py
@@ -1,20 +1,22 @@
+# Path: crews/coding/crew.py
+
 from crewai import Crew, Process
 from typing import Dict, Any
 from langchain_community.llms.ollama import Ollama
 from config import config
-
 from .agents import create_coding_developer_agent, create_qa_engineer_agent
-from .tasks import create_coding_task, create_review_task
+from .tasks import create_coding_task
 
-def create_coding_crew(llm: Ollama, inputs: Dict[str, Any]) -> Crew:
-    coder = create_coding_developer_agent(llm, inputs)
+# Add `full_output` to the function signature with a default value of False
+def create_coding_crew(llm: Ollama, inputs: Dict[str, Any], full_output: bool = False) -> Crew:
+    coding_developer = create_coding_developer_agent(llm, inputs)
     qa_engineer = create_qa_engineer_agent(llm, inputs)
-    coding_task = create_coding_task(coder, inputs)
-    review_task = create_review_task(qa_engineer, inputs)
+    coding_task = create_coding_task(coding_developer, inputs)
 
     return Crew(
-        agents=[coder, qa_engineer],
-        tasks=[coding_task, review_task],
+        agents=[coding_developer, qa_engineer],
+        tasks=[coding_task],
+        process=Process.sequential,
         verbose=config.agents.verbose,
-        full_output = full_output  # Pass the argument to the Crew constructor
+        full_output=full_output # Pass the argument to the Crew constructor
     )
--- src/crews/math/crew.py
+++ src/crews/math/crew.py
@@ -1,21 +1,18 @@
-# Path: crews/coding/crew.py
-
 from crewai import Crew, Process
 from typing import Dict, Any
 from langchain_community.llms.ollama import Ollama
 from config import config
-from .agents import create_coding_developer_agent, create_qa_engineer_agent
-from .tasks import create_coding_task
+from .agents import create_mathematician_agent
+from .tasks import create_math_task
 
 # Add `full_output` to the function signature with a default value of False
-def create_coding_crew(llm: Ollama, inputs: Dict[str, Any], full_output: bool = False) -> Crew:
-    coding_developer = create_coding_developer_agent(llm, inputs)
-    qa_engineer = create_qa_engineer_agent(llm, inputs)
-    coding_task = create_coding_task(coding_developer, inputs)
+def create_math_crew(llm: Ollama, inputs: Dict[str, Any], full_output: bool = False) -> Crew:
+    mathematician = create_mathematician_agent(llm, inputs)
+    math_task = create_math_task(mathematician, inputs)
 
     return Crew(
-        agents=[coding_developer, qa_engineer],
-        tasks=[coding_task],
+        agents=[mathematician],
+        tasks=[math_task],
         process=Process.sequential,
         verbose=config.agents.verbose,
         full_output=full_output # Pass the argument to the Crew constructor
