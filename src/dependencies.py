287ef2de330ec29c738875c5666220e976b12386
Allen Ford <allen@cyfordtechnologies.com>
2025-08-28 21:54:47 -0400

    fixing api

--- src/dependencies.py
+++ src/dependencies.py
@@ -3,7 +3,7 @@
 # Import all modules here
 from peer_discovery import peer_discovery
 from distributed_router import DistributedRouter
-from ai_crew import AICrewManager
+#from ai_crew import AICrewManager
 from cache_manager import cache
 
 # Create single instances of shared classes
