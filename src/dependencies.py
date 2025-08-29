96b4e40dd562ff6461116e433e777d4fe34d7a2e
Allen Ford <allen@cyfordtechnologies.com>
2025-08-28 21:56:44 -0400

    fixing api

--- src/dependencies.py
+++ src/dependencies.py
@@ -4,7 +4,10 @@
 from peer_discovery import peer_discovery
 from distributed_router import DistributedRouter
 #from ai_crew import AICrewManager
-from cache_manager import cache
+# Import AICrewManager lazily
+def get_ai_crew_manager():
+    from ai_crew import AICrewManager
+    return AICrewManager
 
 # Create single instances of shared classes
 distributed_router_instance = DistributedRouter(peer_discovery)
