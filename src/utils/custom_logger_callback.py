0d1027405ce781de59f372da165ba07f43b71616
Allen <allen@cyfordtechnologies.com>
2025-08-31 08:50:17 -0400

    testing

--- src/utils/custom_logger_callback.py
+++ src/utils/custom_logger_callback.py
@@ -2,7 +2,7 @@ import json
 from datetime import datetime
 
 # --- REVISED IMPORT ---
-from crewai.callbacks import Callback
+from crewai.tools.callbacks import Callback
 from rich.console import Console
 
 console = Console()
