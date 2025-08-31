e2416f9258dabc9994e1b4920a795e3de42808a3
Allen Ford <allen@cyfordtechnologies.com>
2025-08-30 21:26:56 -0400

    adding  and repairing crew

--- src/crews/internal/team_manager/__init__.py
+++ src/crews/internal/team_manager/__init__.py
@@ -1,6 +1,9 @@
 # src/crews/internal/team_manager/__init__.py
 
+from .agents import ErrorLogger, AVAILABLE_AGENTS, format_agent_list
 from .crew import get_team_manager_crew
-from .agent import ErrorLogger, AVAILABLE_AGENTS, format_agent_list
 
+# You might want to define which functions or classes to expose
+# from the package using __all__ for cleaner imports.
 __all__ = ["get_team_manager_crew", "ErrorLogger", "AVAILABLE_AGENTS", "format_agent_list"]
+", "format_agent_list"]
