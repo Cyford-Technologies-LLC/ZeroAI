8e1c2dfe4d40b1b9bf2086d064ec05478bbf076b
Allen Ford <allen@cyfordtechnologies.com>
2025-08-31 22:05:27 -0400

    fixing crew communication

--- src/crews/internal/team_manager/__init__.py
+++ src/crews/internal/team_manager/__init__.py
@@ -1,7 +1,6 @@
 # src/crews/internal/team_manager/__init__.py
 
 from .agents import ErrorLogger, AVAILABLE_AGENTS, format_agent_list
-from .crew import get_team_manager_crew
 
 # You might want to define which functions or classes to expose
 # from the package using __all__ for cleaner imports.
