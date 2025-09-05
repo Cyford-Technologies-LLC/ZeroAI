cb45e9595246279f9d7a2b87cac41198797d9c3a
Allen <allen@cyfordtechnologies.com>
2025-09-05 11:43:07 -0400

    fixing knowledge code

--- src/utils/knowledge_utils.py
+++ src/utils/knowledge_utils.py
@@ -3,7 +3,7 @@ import yaml
 from typing import List, Tuple
 from crewai.knowledge.source.string_knowledge_source import StringKnowledgeSource
 from langchain_ollama import OllamaEmbeddings
-
+os.environ['OLLAMA_HOST'] = "http://149.36.1.65:11434"
 # Define the Ollama embedder and point to your local endpoint.
 ollama_embedder = OllamaEmbeddings(
     model="nomic-embed-text",
