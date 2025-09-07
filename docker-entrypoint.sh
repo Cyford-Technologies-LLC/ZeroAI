ca34be01a4e1b2b800f62eca6bc31b93be54e4d9
Allen <allen@cyfordtechnologies.com>
2025-09-07 12:22:39 -0400

    fixing knowledge code,  adding location to docker file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Source the user's profile to ensure PATH is correctly set
+. "$HOME/.profile"
+
 # Change ownership of the config directory to the appuser
 chown -R appuser:appuser /app/
 #chown -R appuser:appuser /app/config
