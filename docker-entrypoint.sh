dc95eeffdaf1101c4414697f6982223f37938f63
Allen <allen@cyfordtechnologies.com>
2025-09-07 12:01:22 -0400

    fixing knowledge code,  adding location to docker file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Change ownership of the config directory to the appuser
-chown -R appuser:appuser /app/config
+chown -R appuser:appuser /app/
+#chown -R appuser:appuser /app/config
 
 # Execute the original command
 exec "$@"
