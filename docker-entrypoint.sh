24afe3b8d0329ab3e6f6e9ba5bacf8b6e4b6405b
Allen <allen@cyfordtechnologies.com>
2025-09-07 14:56:26 -0400

    adding crewai learning tools

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-# Set defaults for UID and GID for portability.
-export EXEC_UID=${LOCAL_UID:-999}
-export EXEC_GID=${LOCAL_GID:-999}
+# Set the UID and GID for the application process.
+# These values are passed from docker-compose.yml and default to 999.
+EXEC_UID=${LOCAL_UID:-999}
+EXEC_GID=${LOCAL_GID:-999}
 
-# **FIX:** Ensure the entire /app directory is owned by the appuser
-chown -R "$LOCAL_UID":"$LOCAL_GID" /app
-
-# The DOCKER_HOST environment variable should be set to the path of the mounted socket.
-# Ensure that the appuser can access the Docker socket
-#if [ -S "/var/run/docker.sock" ]; then
-#    chmod 666 /var/run/docker.sock
-#fi
-
+# Log the UID and GID being used
 echo "Running as UID: ${EXEC_UID}, GID: ${EXEC_GID}"
 
-# Execute the main application command as the appuser
+# Execute the main application command as the specified user.
+# The user's UID and GID are set to match the host's via the entrypoint.
 exec gosu "$EXEC_UID:$EXEC_GID" "$@"
