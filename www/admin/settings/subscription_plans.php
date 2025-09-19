6e0522cc5b2ba9b891654404ad4de56502eeec75
Allen <allen@cyfordtechnologies.com>
2025-09-19 02:27:34 -0400

    Dynamic menu responsive

--- www/admin/settings/subscription_plans.php
+++ www/admin/settings/subscription_plans.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $pageTitle = 'Subscription Plans Management';
 include __DIR__ . '/../includes/header.php';
 
