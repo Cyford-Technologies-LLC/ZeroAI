733c6237923811896253cfc5d5ccb13bfd2ea241
Allen <allen@cyfordtechnologies.com>
2025-09-19 03:39:04 -0400

    Dynamic menu responsive

--- www/admin/settings/subscription_plans.php
+++ www/admin/settings/subscription_plans.php
@@ -429,6 +429,7 @@ try {
 
 <script>
 function editPlan(id) {
+    alert('Edit clicked for plan ' + id);
     console.log('Edit plan clicked:', id);
     const button = document.querySelector(`button[onclick="editPlan(${id})"]`);
     const planData = JSON.parse(button.getAttribute('data-plan'));
