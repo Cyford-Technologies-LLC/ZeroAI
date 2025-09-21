5c25aa03cef6d38cc0e9c6faa66ba2bc8fd999b8
Allen <allen@cyfordtechnologies.com>
2025-09-21 10:06:55 -0400

    Adding Avatar Video working,  audio not

--- avatar/rebuild.sh
+++ avatar/rebuild.sh
@@ -1,9 +1,9 @@
-docker system prune --volumes
 cd /opt/cyford/ZeroAI/
 git reset --hard
 git pull
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml stop avatar
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml rm -f avatar
+docker system prune --volumes
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml build --no-cache avatar
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml up -d avatar
 #docker logs zeroai_avatar --follow
