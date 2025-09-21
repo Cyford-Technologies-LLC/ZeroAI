43ef0012513af51fbf5785e62132cc47dec0f7f9
Allen <allen@cyfordtechnologies.com>
2025-09-21 12:31:43 -0400

    Adding Avatar seperating tts

--- avatar/rebuild.sh
+++ avatar/rebuild.sh
@@ -3,6 +3,7 @@ git reset --hard
 git pull
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml stop avatar
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml rm -f avatar
+docker image prune -a
 docker system prune --volumes
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml build --no-cache avatar
 docker compose -f Docker-compose.yml -f docker-compose.gpu.override.yml up -d avatar
