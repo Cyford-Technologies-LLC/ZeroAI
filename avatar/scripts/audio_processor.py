5ed9de8e17617dfd59e2c289c61ab96b7f968b11
Allen <allen@cyfordtechnologies.com>
2025-09-28 14:25:45 -0400

    fixing avatar (avtar - dbug changes)

--- avatar/scripts/audio_processor.py
+++ avatar/scripts/audio_processor.py
@@ -10,7 +10,7 @@ import requests
 import subprocess
 import os
 import tempfile
-from typing import Dict, Optional, TupleList
+from typing import Dict, Optional, Tuple, List
 from utility import clean_text
 
 # Third-party imports (install with: pip install pydub)
